8e86a88cf5c483e67ea3804b31ed34d0cc86b021
Perego98 <peregopr@gmail.com>
2017-01-20 18:18:47 +0100

    schermo lcd, RTC, bluetooth

--- Materiale/Arduino/Display.ino
+++ Materiale/Arduino/Display.ino
@@ -1,9 +1,16 @@
+#include <SoftwareSerial.h>
 #include <LiquidCrystal.h>  
 #include <DS3232RTC.h>    
 #include <TimeLib.h>         
 #include <Wire.h>      
-      
+
+int rxPin = 6;
+int txPin = 7;
+SoftwareSerial bluetooth(rxPin, txPin);
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);  
+
+String message=""; 
+int conta=0;
   
 void setup() {  
    //impostiamo il numero di colonne ed il numero di righe di lcd  
@@ -11,35 +18,64 @@ void setup() {
   lcd.begin(16, 2);  
   // Visualizzo il messaggio sul display  
    Serial.begin(9600);
+    bluetooth.begin(9600);
     setSyncProvider(RTC.get);   // the function to get the time from the RTC
     if(timeStatus() != timeSet) 
         Serial.println("Unable to sync with the RTC");
     else
         Serial.println("RTC has set the system time"); 
-  lcd.print("Salve mondo!");  
+ // lcd.print("Salve mondo!");
+ lcd.setCursor(0, 0); 
+   // lcd.print('/');
+    lcd.print(day());
+    lcd.print('/');
+    lcd.print(month());
+    lcd.print('/');
+    lcd.print(year()); 
+    //lcd.println(); 
+  lcd.setCursor(0,1);
+  lcd.print("Psw:");  
 }  
   
 void loop() {  
   // posiziona il cursore in colonna 0 e linea 1  
-  // (nota: la linea 1 e la seconda linea, poichè si conta incominciando da 0):  
-  lcd.setCursor(0, 1);  
+  // (nota: la linea 1 e la seconda linea, poichè si conta incominciando da 0):
+
+ while(bluetooth.available()){
+   message+=char(bluetooth.read());
+   conta++;    
+  }
+  if(!bluetooth.available())
+  {
+    if(message!="")
+    {//if data is available
+       lcd.setCursor(10, 0);  
   // stampa il numero di secondi dall'ultimo reset  
-  digitalClockDisplay();  
-    delay(1000); 
+  digitalClockDisplay();
+
+  lcd.setCursor(0,1);
+  lcd.print("Psw:");
+  lcd.print(message);
+  
+     if(conta>8){
+      lcd.setCursor(4,5);
+      lcd.print("            ");
+      message=""; //clear the data
+     conta=0;
+     }
+     
+    }
+  }
+  delay(1000);     
+   
 }  
 void digitalClockDisplay(void)
 {
     // digital clock display of the time
     lcd.print(hour());
     printDigits(minute());
-    printDigits(second());
-    lcd.print(' ');
-    lcd.print(day());
-    lcd.print(' ');
-    lcd.print(month());
-    lcd.print(' ');
-    lcd.print(year()); 
-    lcd.println(); 
+    //printDigits(second());
+    
 }
 
 void printDigits(int digits)
